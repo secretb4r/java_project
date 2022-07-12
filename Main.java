03d10dd557142db3fa7d7344a79a7eb9481e4d06
secretb4r <107612318+secretb4r@users.noreply.github.com>
2022-07-12 11:06:39 +0000

    Create Main.java
    First new java file.

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+   public static void main(String[] args){
+     System.out.println("I am nwew in Java");
+     System.out.println("I am new in github");
+     System.out.println("i am also new in github codespace");
+   }
+
+}
